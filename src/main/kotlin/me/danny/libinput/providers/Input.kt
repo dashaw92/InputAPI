e319d2688f343f6de44571896e7c7fb6615966f2
dashaw92 <dashaw92@gmail.com>
2023-01-25 17:20:57 -0500

    Add conversion methods for QoL

--- src/main/kotlin/me/danny/libinput/providers/Input.kt
+++ src/main/kotlin/me/danny/libinput/providers/Input.kt
@@ -2,5 +2,10 @@ package me.danny.libinput.providers
 
 sealed interface Input
 
-data class SingleLine(val line: String?) : Input
-data class MultipleLines(val lines: List<String?>) : Input
+data class SingleLine(val line: String) : Input {
+    fun toMultiple(): MultipleLines = MultipleLines(listOf(line))
+}
+
+data class MultipleLines(val lines: List<String>) : Input {
+    fun toSingle(joiner: (List<String>) -> String): SingleLine = SingleLine(joiner(lines))
+}
